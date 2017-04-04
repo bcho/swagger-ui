a518cd0f196b44211a57b9c103f0ae1d7f26e401
hbc <bcxxxxxx@gmail.com>
2017-04-04 21:37:00 +0800

    fix: hide swagger logo
    oh that's evil

--- src/plugins/topbar/topbar.jsx
+++ src/plugins/topbar/topbar.jsx
@@ -40,8 +40,6 @@ export default class Topbar extends React.Component {
           <div className="wrapper">
             <div className="topbar-wrapper">
               <Link href="#" title="Swagger UX">
-                <img height="30" width="30" src={ Logo } alt="Swagger UX"/>
-                <span>swagger</span>
               </Link>
               <div className="download-url-wrapper">
                 <input className="download-url-input" type="text" onChange={ this.onUrlChange } value={this.state.url} disabled={isLoading} style={inputStyle} />
